1394ed0269a6091a714783930d40c4a272ef1002
Yoda <dyoungdal@gmail.com>
2024-06-04 18:27:05 +0800

    [IMP] add user controllers

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,37 @@
+class UsersController < ApplicationController
+    skip_before_action :verify_authenticity_token, only: [:destroy]
+    before_action :set_user, only: [:destroy]
+  
+    def index
+        @users = User.all
+        @users = @users.where("name->>'first' ILIKE :search OR name->>'last' ILIKE :search", search: "%#{params[:search]}%") if params[:search].present?
+
+        template = File.read(Rails.root.join("app", "views", "users/index.html.liquid"))
+
+        render :inline => Liquid::Template.parse(template).render('users' => @users.map(&:attributes))
+    end
+  
+    def destroy
+      @user.destroy
+      # Update DailyRecord count
+      update_daily_record_count(@user.gender)
+    end
+  
+    private
+  
+    def set_user
+      @user = User.find(params[:id])
+    end
+  
+    def update_daily_record_count(gender)
+      daily_record = DailyRecord.find_by(date: Date.today)
+      if daily_record
+        if gender == 'male'
+          daily_record.update(male_count: daily_record.male_count - 1)
+        elsif gender == 'female'
+          daily_record.update(female_count: daily_record.female_count - 1)
+        end
+      end
+    end
+  end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root 'users#index'
+
+  resources :users
 end
